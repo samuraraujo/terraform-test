ae9479a1080fc9d5d93c44c280c5c0fe563c2237
Samur Araujo, PhD <samuraraujo@gmail.com>
2023-03-16 21:56:04 +0100

    load balancer

--- main.tf
+++ main.tf
@@ -55,3 +55,45 @@ module "blog_security_group" {
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "my-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = module.blog_security_group.security_group_id
+ 
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = "iaws_instance.blog.id"
+          port = 80
+        }
+       
+      }
+    }
+  ] 
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
