0a21bd76331bcc4681428de4292728274bc2e190
Samur Araujo, PhD <samuraraujo@gmail.com>
2023-03-16 22:01:44 +0100

    fix target_id

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ module "alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = "iaws_instance.blog.id"
+          target_id = "aws_instance.blog.id"
           port = 80
         }
        
