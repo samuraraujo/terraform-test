8cd277c09f3f54e7ef031cc3b66a344fdfa1ef14
Samur Araujo, PhD <samuraraujo@gmail.com>
2023-03-16 22:11:06 +0100

    fix typos

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -32,12 +32,12 @@ module "vpc" {
 }
 
 
-module "blog_security_group" {
+module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
   name = "blog_new"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -50,8 +50,8 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   
-  subnet_id = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_security_group.security_group_id]
+  subnet_id = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
@@ -66,9 +66,9 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.vpc.public_subnets
-  security_groups    = [module.blog_security_group.security_group_id]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sp.security_group_id]
  
 
   target_groups = [
